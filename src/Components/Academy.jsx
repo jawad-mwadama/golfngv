e19b7341bb73a829de9ae073e9333a3a4fe53291
jawad-mwadama <allymwadama@gmail.com>
2025-09-03 23:59:09 +0300

    made a continuation of the academy section

--- src/Components/Academy.jsx
+++ src/Components/Academy.jsx
@@ -3,8 +3,17 @@ import React from "react";
 function Academy() {
   return (
     <div className="pt-6 sm:pt-9">
-      <div className="flex items-center justify-center p-9 sm:p-6">
-        <p className="text-sm font-semibold text-lime-200">Gold Academy</p>
+      <div className="mx-auto flex flex-col items-center justify-center p-9 sm:p-6">
+        <p className="text-sm font-semibold text-lime-300">Gold Academy</p>
+
+        <h2 className="pt-3 text-4xl font-semibold tracking-wide">
+          Professional Coaching & Golf Academy
+        </h2>
+
+        <p className="p-2 text-center text-sm text-zinc-300">
+          Locally sourced ingridients, gourmet meals, and sunset views. Latest
+          golf gear,branded <br /> apparel and expert fitting services
+        </p>
       </div>
     </div>
   );
