28041bef79b3ada1558a5564c969dbfe229ffc03
jawad-mwadama <allymwadama@gmail.com>
2025-09-10 23:57:06 +0300

    changes to mobile responsiveness on hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,22 +13,22 @@ const tabs = [
 function Hero() {
   return (
     <div className="relative flex h-screen flex-wrap justify-between rounded-3xl bg-[url('/images/scene-with-friends-golf-course.jpg')] mask-b-from-80% mask-b-to-100% bg-cover bg-center bg-no-repeat p-3 sm:p-9">
-      <div>
+      <div className="">
         {/* changed the bottom and top, come bakc to it if there are issues */}
-        <p className="absolute bottom-[60%] space-y-1 rounded-full bg-lime-200 p-1.5 font-light text-zinc-400 sm:bottom-[55%] sm:font-medium">
+        <p className="absolute bottom-[45%] space-y-1 rounded-full bg-lime-200 p-1.5 font-light text-zinc-400 sm:bottom-[47%] sm:font-medium">
           Version 2.0 is here
           <span className="px-2 text-xs text-black">Read Here &rarr;</span>
         </p>
-        <h2 className="absolute bottom-74 text-4xl font-semibold text-white sm:bottom-60 md:text-7xl">
+        <h2 className="absolute bottom-[32%] text-4xl font-semibold text-white sm:bottom-[30%] md:text-7xl">
           Unwind, Play, and <br /> Perfect Your Swing
         </h2>
         {/* adjusted tracking font weight and removed the br tag */}
-        <p className="absolute bottom-50 font-light tracking-tight text-slate-50 sm:bottom-42 sm:tracking-wide">
+        <p className="absolute bottom-[19%] font-light tracking-tight text-slate-50 sm:bottom-[22%] sm:tracking-wide">
           A championship experience awaits you on lush, world-class <br />
           fairways. Your next legendary round starts here.
         </p>
 
-        <div className="absolute bottom-30 flex gap-5 sm:bottom-20">
+        <div className="absolute bottom-[9%] flex gap-5 sm:bottom-[13%]">
           <JoinButton />
           <DiscoverButton />
         </div>
@@ -43,7 +43,7 @@ function Hero() {
 function Tab() {
   const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-    <div className="absolute right-6 hidden gap-2 md:bottom-20 md:grid md:grid-cols-3">
+    <div className="absolute right-6 hidden gap-2 md:bottom-[10%] md:grid md:grid-cols-3">
       {tabs.map((tab) => (
         <button
           key={tab.id}
